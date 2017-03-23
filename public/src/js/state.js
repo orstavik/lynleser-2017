644844c31a46009259f2c6d07688077954ab4d01
orstavik <orstavik77@gmail.com>
2017-03-23 17:08:14 +0100

    following the path of clojure.. hopefully they are better plumbers..

--- public/src/js/state.js
+++ public/src/js/state.js
@@ -1,46 +1,32 @@
-class State {
-  constructor(settings, books) {
-    this.books = books || {};
-    this.settings = settings || {};
-    this.activeBook = this.settings.activeBook ? this.books[this.settings.activeBook] : undefined;
-  }
-
-  merge(B) {
-    if (!B)
-      return this;
-    return new State(
-      Tools.merge1(this.settings, B.settings),
-      Tools.merge2(this.books, B.books)
-    );
-  }
-
-  newSetting(key, value) {
-    const clone = new State(Object.assign({}, this.settings), this.books);
-    clone.settings[key] = value;
-    return clone;
-  }
-
-  //todo this should be made immutable.
-  filterUnionBranches(B) {
-    return Tools.filter1Level(this, B);
-  }
-
-  isEmpty() {
-    return Object.keys(this.settings).length === 0 && Object.keys(this.books).length === 0;
-  }
-
-  nextBookPosition() {
-    if (!this.activeBook)
-      return undefined;
-    const res = new State();
-    const activeInList = res.books[this.settings.activeBook] = {};
-    activeInList.position = (this.activeBook.position || 0) + 1;
-    return res;
-  }
-
-  newSettingRes(key, value) {
-    const res = new State();
-    res.settings[key] = value;
-    return res;
-  }
-}
+// class State {
+//   constructor(settings, books) {
+//     this.books = books || {};
+//     this.settings = settings || {};
+//     this.activeBook = this.settings.activeBook ? this.books[this.settings.activeBook] : undefined;
+//   }
+//
+//   newSetting(key, value) {
+//     const clone = new State(Object.assign({}, this.settings), this.books);
+//     clone.settings[key] = value;
+//     return clone;
+//   }
+//
+//   static isEmpty(A) {
+//     return Object.keys(A.settings).length === 0 && Object.keys(A.books).length === 0;
+//   }
+//
+//   nextBookPosition() {
+//     if (!this.activeBook)
+//       return undefined;
+//     const res = new State();
+//     const activeInList = res.books[this.settings.activeBook] = {};
+//     activeInList.position = (this.activeBook.position || 0) + 1;
+//     return res;
+//   }
+//
+//   newSettingRes(key, value) {
+//     const res = new State();
+//     res.settings[key] = value;
+//     return res;
+//   }
+// }
--- public/src/js/tools.js
+++ public/src/js/tools.js
@@ -4,14 +4,14 @@
 class Tools {
 
   static merge1(A, B) {
-    if (A == B) return A;
+    if (A === B) return A;
     if (!B) return A;
     if (!A) return B;
     return Object.assign({}, A, B);
   }
 
   static merge2(A, B) {
-    if (A == B) return A;
+    if (A === B) return A;
     if (!B || Object.keys(B).length === 0) return A;
     if (!A || Object.keys(A).length === 0) return B;
     const C = Object.assign({}, A);
@@ -20,62 +20,144 @@ class Tools {
     return C;
   }
 
+  static merge(A, B) {
+    if (!B) return A;
+    if (!A) return B;
+    return {
+      settings: Tools.merge1(A.settings, B.settings),
+      books: Tools.merge2(A.books, B.books)
+    };
+  }
+
+  // static isEmpty(A) {
+  //   return Object.keys(A.settings).length === 0 && Object.keys(A.books).length === 0;
+  // }
+
+
   //it should remove all branches from A that is already in B
   //todo make it immutable
   //todo make this general and multilevel
-  static filter1Level(A, B) {
-    for (let key of Object.keys(B)) {
-      let b = B[key];
-      let a = A[key];
-      if (!a || !b)
-        continue;
-      for (let s of Object.keys(b)) {
-        if (JSON.stringify(b[s]) === JSON.stringify(a[s]))
-          delete a[s];
-      }
-    }
-  }
+  // static filter1Level(A, B) {
+  //   for (let key of Object.keys(B)) {
+  //     let b = B[key];
+  //     let a = A[key];
+  //     if (!a || !b)
+  //       continue;
+  //     for (let s of Object.keys(b)) {
+  //       if (JSON.stringify(b[s]) === JSON.stringify(a[s]))
+  //         delete a[s];
+  //     }
+  //   }
+  // }
 
   /**
-   * Flattens a normal object tree. Does not work with arrays.
+   * Flattens a normal object tree to an array of {path, value} objects
+   * where path is an array of keys as strings. Only works with objects.
    *
    *     let tree = {a: {x: 1}, b: {y: {"12": "something"}}};
-   *     let flatTree = State.flatten(tree, ".");
-   *     flatTree == {"a.x" : 1, "b.y.12": "something"}; //true
+   *     let flatTree = Tools.flatten(tree);
+   *     //[{path: ["a","x"], value: 1}, {path: ["b","y","12"], value: "something"}]
    *
-   * @param normalObject object to be flattened
-   * @param separator "." for json, "/" for firebase
-   * @returns a new flattened object with paths as keys.
+   * @param object object to be flattened
+   * @returns an array of [{path: <array>, value: ?}] for that object
    */
-  static flatten(normalObject, separator) {
-    const result = {};
-    Tools._flattenImpl(normalObject, "", separator, result);
-    return result;
+  static flatten(object) {
+    const res = []; //mutable
+    Tools._flattenImpl(object, [], res);
+    return res;
   }
 
-  static _flattenImpl(obj, startPath, separator, flattenedObject) {
-    if (obj instanceof Object) {
-      for (let childPath of Object.keys(obj))
-        Tools._flattenImpl(obj[childPath], startPath + separator + childPath, separator, flattenedObject);
-    } else
-      flattenedObject[startPath] = obj;
+  static _flattenImpl(obj, path, res) {
+    if (!(obj instanceof Object)) {
+      res.push({path: path, value: obj});
+      return;
+    }
+    for (let key of Object.keys(obj))
+      Tools._flattenImpl(obj[key], path.concat([key]), res);
   }
 
   /**
    * adds a startPath to all keys
    *
    * let flat = {"a/b": 1, c: 2, "xyz/12": 3};
-   * let extendedFlat = State.extendKeys("new/root/", flat);
+   * let extendedFlat = State.flatToObject("new/root/", flat);
    * extendedFlat == {"new/root/a/b": 1, "new/root/c": 2, "new/root/xyz/12": 3}; //true
    *
+   * @param flat an array of a flattened object
    * @param {string} startPath
-   * @param object a flattened object is probably best
    * @returns a new object with extended key names.
    */
-  static extendKeys(startPath, object) {
+  static flatToObject(flat, startPath, separator) {
     let result = {};
-    for (let key of Object.keys(object))
-      result[startPath + key] = object[key];
+    for (let pathValue of flat)
+      result[startPath + pathValue.path.join(separator)] = pathValue.value;
     return result;
+  }
+
+  static setIn(obj, path, value) {
+    return Tools._getInImpl(obj, path, 0) === value ? obj : Tools._setInImpl(obj, path, value, 0);
+  }
+
+  static _setInImpl(obj, path, value, pos) {
+    if (path.length <= pos)
+      return value;
+    if (!(obj instanceof Object))
+      obj = {}; //we overwrite all none object values by default
+    const key = path[pos];
+    const clone = Object.assign({}, obj);
+    clone[key] = Tools._setInImpl(obj[key], path, value, ++pos);
+    return clone;
+  }
+
+  static getIn(obj, path) {
+    return Tools._getInImpl(obj, path, 0);
+  }
+
+  static _getInImpl(obj, path, pos) {
+    if (path.length <= pos)
+      return obj;
+    if (!obj)
+      return undefined;
+    const key = path[pos];
+    return Tools._getInImpl(obj[key], path, ++pos);
+  }
+
+  /**
+   * Immutable filter that strips out
+   * 1) entries of A that are matching exactly entries in B
+   * 2) all empty entries (with undefined as value).
+   *
+   * @param {object} A the thing to be filtered
+   * @param {object} B the filter
+   * @returns A if nothing is filtered away,
+   *          undefined if A is empty or the whole content of A is filtered out by B,
+   *          a new object C which is an immuted version of the partially filtered A.
+   */
+  static filterDeep(A, B) {
+    if (A === B)
+      return undefined;
+    if (B === undefined /*|| B === null*/)
+      return A;
+    if (!(A instanceof Object) || !(B instanceof Object))
+      return A;
+    if (Object.keys(A).length === 0)
+      return undefined;
+
+    const C = {};
+    let hasFiltered = false;
+    for (let key of Object.keys(A)) {
+      const a = A[key];
+      const b = B[key];
+      let c = Tools.filterDeep(a, b);
+      if (c !== a)
+        hasFiltered = true;
+      if (c !== undefined /*&& c !== null*/)
+        C[key] = c;
+    }
+    if (!hasFiltered)
+      return A;
+    if (Object.keys(C).length === 0)
+      return undefined;
+    return C;
   }
 }
