923af6ee9785ed0f7d2cf7ee005e63c6dade3a24
enlightenmentor <fales.tom.1994@gmail.com>
2017-04-06 16:58:10 +0300

    many many changes

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
 importScripts('/src/js/sw-lib.min.js');
-/*
+
 const cdnCacheStrategy = goog.swlib.cacheFirst({cacheableResponse: {statuses: [0],}});
 goog.swlib.router.registerRoute(new RegExp('^https://unpkg.com/compromise@latest/builds/compromise.min.js'), cdnCacheStrategy);
 goog.swlib.router.registerRoute(new RegExp('^https://cdn.jsdelivr.net/lodash'), cdnCacheStrategy);
 goog.swlib.router.registerRoute(new RegExp('^https://www.gstatic.com/firebasejs/3.7.0/firebase.js'), cdnCacheStrategy);
+goog.swlib.router.registerRoute(new RegExp('^https://firebasestorage.googleapis.com/v0/b/lynleser-2017.appspot.com/o/books'), cdnCacheStrategy);
 goog.swlib.router.registerRoute('/sw.js', goog.swlib.networkFirst());         //.staleWhileRevalidate() ??
 goog.swlib.router.registerRoute('/', goog.swlib.networkFirst());              //.staleWhileRevalidate() ??
 goog.swlib.router.registerRoute('/index.html', goog.swlib.networkFirst());    //.staleWhileRevalidate() ??
 goog.swlib.router.registerRoute(new RegExp('.html|.css|.js'), goog.swlib.cacheFirst());
-*/
